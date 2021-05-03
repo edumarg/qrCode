73649d42ba9efe4dcd3cbad13fbc65b844f7ecab
Eduardo Margulis <edumarg@gmx.com>
2021-05-03 12:04:04 +0300

    added logic to create basic qrcode

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def print_hi(name):
-    print(f'Hi {name}')
+import qrcode
+import os
+
+
+def create_qrcode(data):
+    img = qrcode.make(data)
+    current_path = os.path.abspath(os.getcwd())
+    img.save(f"{current_path}\myqrcode.png")
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+    qrcode_data = input("Enter data to encode in the qrcode:\n>>")
+    create_qrcode(qrcode_data)
